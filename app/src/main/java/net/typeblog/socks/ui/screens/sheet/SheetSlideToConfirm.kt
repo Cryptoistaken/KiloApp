25659ed4c8129495340b014915a087e156006444
Cryptoistaken <traderspopy@gmail.com>
2026-09-22 19:27:56 +0600

    Fix focusable import package for slide-to-confirm

--- app/src/main/java/net/typeblog/socks/ui/screens/sheet/SheetSlideToConfirm.kt
+++ app/src/main/java/net/typeblog/socks/ui/screens/sheet/SheetSlideToConfirm.kt
@@ -2,6 +2,7 @@ package net.typeblog.socks.ui.screens.sheet
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -27,7 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.focusable
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
