3bbb1f64d65175b2e3f92cab05579025d37627f1
Cryptoistaken <traderspopy@gmail.com>
2026-09-23 06:34:30 +0600

    SelectBottomBar: drop heavy shadow, keep border

--- app/src/main/java/net/typeblog/socks/ui/screens/sheet/SheetMultiSelect.kt
+++ app/src/main/java/net/typeblog/socks/ui/screens/sheet/SheetMultiSelect.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -110,7 +109,6 @@ fun SelectBottomBar(
     ) {
         Row(
             modifier = Modifier
-                .shadow(8.dp, androidx.compose.foundation.shape.RoundedCornerShape(20.dp))
                 .clip(androidx.compose.foundation.shape.RoundedCornerShape(20.dp))
                 .background(MaterialTheme.colorScheme.surface)
                 .border(
